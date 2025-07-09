ad4707fe443e529a139e44428347fd76dec27198
QuatroFiend <buddahvamp@gmail.com>
2025-07-09 20:50:08 +0200

    ui upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import s from './style.module.css';
+import { ReactComponent as Telegram } from "./img/Telegram.svg";
+import { ReactComponent as GitHub } from "./img/GitHub.svg";
+import { ReactComponent as Linkedin } from "./img/Linkedin.svg";
+import { ReactComponent as CV } from "./img/cv.svg";
+import { ReactComponent as Web } from "./img/website.svg";
+import avatar from "./img/avatar.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className={"gradient-background"}>
+            <div className="App">
+                <header className="App-header">
+                    <div className={s.avatar}>
+                        <img alt="" src={avatar}/>
+                    </div>
+                    <span className={s.userName}>QuatroFiend</span>
+                    <div className={s.buttonsBlock}>
+                        <button className={s.linkedButton}>
+                            <div className={s.buttonContent}>
+                                <Linkedin className={s.icon}/> <a className={s.link}
+                                                                   href={"https://www.linkedin.com/in/olek-salnikał-337315318/"}>Linkedin</a>
+                            </div>
+                        </button>
+                        <button className={s.linkedButton}>
+                            <div className={s.buttonContent}>
+                                <Telegram className={s.icon}/> <a className={s.link}
+                                                                  href={"https://t.me/quatrodhaoregu"}>Telegram</a>
+                            </div>
+                        </button>
+                        <button className={s.linkedButton}>
+                            <div className={s.buttonContent}>
+                                <GitHub className={s.icon}/> <a className={s.link}
+                                                                href={"https://github.com/QuatroFiend"}>GitHub</a>
+                            </div>
+                        </button>
+                        <button className={s.linkedButton}>
+                            <div className={s.buttonContent}>
+                                <Web className={s.icon}/> <a className={s.link}
+                                                                 href={"https://quatrofiend.github.io/portfolio-start-project-main/"}>Portfolio</a>
+                            </div>
+                        </button>
+                        <button className={s.linkedButton}>
+                            <div className={s.buttonContent}>
+                                <CV className={s.icon}/> <a className={s.link}
+                                                                   href={"https://drive.google.com/file/d/1_3x2FbjS-tCA1AP2MAblTygSUHgoNZOp/view?usp=sharing"}>CV</a>
+                            </div>
+                        </button>
+                    </div>
+                    <span className={`${s.footerSpan} ${s.shineText}`}>第四個惡魔</span>
+                </header>
+            </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
