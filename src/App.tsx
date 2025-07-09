50d0bbec0266ef2493d15ce61d7c8025d6e75661
QuatroFiend <buddahvamp@gmail.com>
2025-07-09 21:27:55 +0200

    href fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,72 @@
 import React from 'react';
 import './App.css';
 import s from './style.module.css';
-import { ReactComponent as Telegram } from "./img/Telegram.svg";
-import { ReactComponent as GitHub } from "./img/GitHub.svg";
-import { ReactComponent as Linkedin } from "./img/Linkedin.svg";
-import { ReactComponent as CV } from "./img/cv.svg";
-import { ReactComponent as Web } from "./img/website.svg";
-import avatar from "./img/avatar.jpg";
+import {ReactComponent as Telegram} from "./img/Telegram.svg";
+import {ReactComponent as GitHub} from "./img/GitHub.svg";
+import {ReactComponent as Linkedin} from "./img/Linkedin.svg";
+import {ReactComponent as CV} from "./img/cv.svg";
+import {ReactComponent as Web} from "./img/website.svg";
+import avatar from "./img/pea.jpg";
 
 function App() {
     return (
         <>
             <div className={"gradient-background"}>
-            <div className="App">
-                <header className="App-header">
-                    <div className={s.avatar}>
-                        <img alt="" src={avatar}/>
-                    </div>
-                    <span className={s.userName}>QuatroFiend</span>
-                    <div className={s.buttonsBlock}>
-                        <button className={s.linkedButton}>
-                            <div className={s.buttonContent}>
-                                <Linkedin className={s.icon}/> <a className={s.link}
-                                                                   href={"https://www.linkedin.com/in/olek-salnikał-337315318/"}>Linkedin</a>
-                            </div>
-                        </button>
-                        <button className={s.linkedButton}>
-                            <div className={s.buttonContent}>
-                                <Telegram className={s.icon}/> <a className={s.link}
-                                                                  href={"https://t.me/quatrodhaoregu"}>Telegram</a>
-                            </div>
-                        </button>
-                        <button className={s.linkedButton}>
-                            <div className={s.buttonContent}>
-                                <GitHub className={s.icon}/> <a className={s.link}
-                                                                href={"https://github.com/QuatroFiend"}>GitHub</a>
-                            </div>
-                        </button>
-                        <button className={s.linkedButton}>
-                            <div className={s.buttonContent}>
-                                <Web className={s.icon}/> <a className={s.link}
-                                                                 href={"https://quatrofiend.github.io/portfolio-start-project-main/"}>Portfolio</a>
-                            </div>
-                        </button>
-                        <button className={s.linkedButton}>
-                            <div className={s.buttonContent}>
-                                <CV className={s.icon}/> <a className={s.link}
-                                                                   href={"https://drive.google.com/file/d/1_3x2FbjS-tCA1AP2MAblTygSUHgoNZOp/view?usp=sharing"}>CV</a>
-                            </div>
-                        </button>
-                    </div>
-                    <span className={`${s.footerSpan} ${s.shineText}`}>第四個惡魔</span>
-                </header>
-            </div>
+                <div className="App">
+                    <header className="App-header">
+                        <div className={s.avatar}>
+                            <img alt="" src={avatar}/>
+                        </div>
+                        <span className={s.userName}>QuatroFiend</span>
+                        <div className={s.buttonsBlock}>
+                            <button className={s.linkedButton}>
+                                <a className={s.link}
+                                   href={"https://www.linkedin.com/in/oleg-salnika%C5%82-976868366/"}>
+                                    <div className={s.buttonContent}>
+                                        <Linkedin className={s.icon}/>
+                                        Linkedin
+                                    </div>
+                                </a>
+                            </button>
+                            <button className={s.linkedButton}>
+                                <a className={s.link}
+                                   href={"https://t.me/quatrodhaoregu"}>
+                                    <div className={s.buttonContent}>
+                                        <Telegram className={s.icon}/>
+                                        Telegram
+                                    </div>
+                                </a>
+                            </button>
+                            <button className={s.linkedButton}>
+                                <a className={s.link}
+                                   href={"https://github.com/QuatroFiend"}>
+                                    <div className={s.buttonContent}>
+                                        <GitHub className={s.icon}/>
+                                        GitHub
+                                    </div>
+                                </a>
+                            </button>
+                            <button className={s.linkedButton}>
+                                <a className={s.link}
+                                   href={"https://quatrofiend.github.io/portfolio-start-project-main/"}>
+                                    <div className={s.buttonContent}>
+                                        <Web className={s.icon}/>
+                                        Portfolio
+                                    </div>
+                                </a>
+                            </button>
+                            <button className={s.linkedButton}>
+                                <a className={s.link}
+                                   href={"https://docs.google.com/document/d/1Tgi6wQrtXuBbedQcYtTaEbgrBGvO-nGqxBewC3JG18A/edit?usp=sharing"}>
+                                    <div className={s.buttonContent}>
+                                        <CV className={s.icon}/>CV
+                                    </div>
+                                </a>
+                            </button>
+                        </div>
+                        <span className={`${s.footerSpan} ${s.shineText}`}>第四個惡魔</span>
+                    </header>
+                </div>
             </div>
         </>
     );
